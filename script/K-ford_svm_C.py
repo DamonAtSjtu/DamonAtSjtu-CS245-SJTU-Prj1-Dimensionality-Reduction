27c011f0c56a0cdde4d21de5edeb60242c8c126b
DamonAtSjtu <42129318+DamonAtSjtu@users.noreply.github.com>
2019-04-08 12:16:53 +0800

    Add files via upload

--- script/K-ford_svm_C.py
+++ script/K-ford_svm_C.py
@@ -39,7 +39,7 @@ print('load successfully->\n')
 
 k_scores = []
 # set your C range here
-c_range = [0.0026, 0.0027, 0.0028, 0.0029 ]
+c_range = [0.0021, 0.0023, 0.0025, 0.0026, 0.0027, 0.0028, 0.0029, 0.003, 0.0032, 0.0035]
 
 for c in c_range:
 
